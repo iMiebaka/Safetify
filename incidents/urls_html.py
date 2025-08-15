7681789046aa4b7e22bd9c131981319be55ef3e4
imiebaka <triple07even@gmail.com>
2025-08-15 15:42:27 +0100

    ⚒️ implement html file abstraction

--- incidents/urls_html.py
+++ incidents/urls_html.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+app_name = "incident_web"
+
 urlpatterns = [
     path("", TemplateView.as_view(template_name="incidents/index.html"), name="incident_index"),
 ]
