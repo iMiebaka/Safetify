c02e47e7898130ec9cde88f40f3915eb05a593c9
imiebaka <triple07even@gmail.com>
2025-08-15 16:47:06 +0100

    🔨 implement project scripting

---
+++ start.sh
@@ -0,0 +1,5 @@
+# Run this with celery stand alone
+
+python mange.py makemigrations
+python mange.py migrate
+python mange.py runserver
---
+++ test.sh
@@ -0,0 +1,3 @@
+# Ensure project is running
+
+docker-compose exec web pytest -q --log-cli-level=INFO
