f50855f7c8b54b101e5679bed4f13c1543229a54
imiebaka <triple07even@gmail.com>
2025-08-17 00:14:02 +0100

    ⚒️ implement frontend url assignment and incident

--- incidents/urls_html.py
+++ incidents/urls_html.py
@@ -5,4 +5,6 @@ app_name = "incident_web"
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="incidents/index.html"), name="incident_index"),
+    path("i/<incident>", TemplateView.as_view(template_name="incidents/incident.html"), name="incident_single"),
+    path("a/<assignment>", TemplateView.as_view(template_name="incidents/assignment.html"), name="assignment_single"),
 ]
